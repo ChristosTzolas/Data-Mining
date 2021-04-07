edf82b4e882a1cc694b182bd5a890120841e8c03
StefanosRigas <stefanosrigas97@gmail.com>
2021-04-08 00:51:16 +0300

    Fixed corellation of smoking and strokes

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ import seaborn as sns
 
 # Create a dataframe for the healthcare dataset
 
-patients_header = ['id', 'gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status', 'stroke']
-patients = pd.read_csv('healthcare-dataset-stroke-data\healthcare-dataset-stroke-data.csv', sep=',', names=patients_header, 
+healthcare_header = ['id', 'gender', 'age', 'hypertension', 'heart_disease', 'ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi', 'smoking_status', 'stroke']
+healthcare = pd.read_csv('healthcare-dataset-stroke-data\healthcare-dataset-stroke-data.csv', sep=',', names=healthcare_header, 
                      usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], encoding="ISO-8859-1", low_memory=False, header=0)
 
 # Print basic info regarding our data (characteristics and possible values)
@@ -20,101 +20,114 @@ patients = pd.read_csv('healthcare-dataset-stroke-data\healthcare-dataset-stroke
 print("\n************ Data Analysis ************\n")
 
 print("The characteristics in our dataset are:\n")
-for col in patients.columns:
+for col in healthcare.columns:
     print(col)
-# or print(patients.columns)    
-print(f"\nSpecifically, there are {len(patients.id.unique())} unique patient ids in the data, of {len(patients.age.unique())} different ages.\n")
+# Alternatively:
+# print(healthcare.columns)    
+print(f"\nSpecifically, there are {len(healthcare.id.unique())} unique ids in the data, of {len(healthcare.age.unique())} different ages.\n")
 
 print("Regarding the possible values of the characteristics, those are:\n")
-print(f"Genders: {(patients.gender.unique())}")
-print(f"Work types: {(patients.work_type.unique())}")
-print(f"Resident types: {(patients.Residence_type.unique())}")
-print(f"Smoking statuses: {(patients.smoking_status.unique())}")
-print(f"BMI range: {min(patients.bmi.unique())} - {max(patients.bmi.unique())}")
-print(f"Average glucose levels range: {min(patients.avg_glucose_level.unique())} - {max(patients.avg_glucose_level.unique())}\n")
+print(f"Genders: {(healthcare.gender.unique())}")
+print(f"Work types: {(healthcare.work_type.unique())}")
+print(f"Resident types: {(healthcare.Residence_type.unique())}")
+print(f"Smoking statuses: {(healthcare.smoking_status.unique())}")
+print(f"BMI range: {min(healthcare.bmi.unique())} - {max(healthcare.bmi.unique())}")
+print(f"Average glucose levels range: {min(healthcare.avg_glucose_level.unique())} - {max(healthcare.avg_glucose_level.unique())}\n")
 print("(The rest of the characteristics have binary values)\n\nSummary Statistics:\n")
 
-# print summary statistics
-print(patients.describe(), "\n")
-
+# Summary statistics
+print(healthcare.describe(), "\n")
 # Count gender instances
 print("Gender Distribution:\n")
-print(patients['gender'].value_counts(), "\n")
-
-print("Mean: {}".format(patients['age'].mean()))
-print("Median: {}".format(patients['age'].median()), "\n")
+print(healthcare['gender'].value_counts(), "\n")
+# Output mean and median age 
+print("Mean age of the dataset: {}".format(healthcare['age'].mean()))
+print("Median age of the dataset: {}".format(healthcare['age'].median()), "\n")
+# Output the top 10 ages present in the dataset
 print("Most common ages:\n")
-print(patients['age'].value_counts().head(10), "\n")
+print(healthcare['age'].value_counts().head(10), "\n")
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 # Correlation between smoking and strokes
 
-# Stroke occurencies among smoking types 
-# We may need to change 0s and 1s to Had/Hadn't Stroke
-for i in patients['smoking_status'].unique():
-     print("Smoking Status: {}\nStroke Occurence:\n{}\n".format(i, patients[patients['smoking_status']==i]['stroke'].value_counts().head(2)))
+# # Stroke occurencies among smoking types 
+# for i in healthcare['smoking_status'].unique():
+#      print("Smoking Status: {}\nStroke Occurence:\n{}\n".format(i, healthcare[healthcare['smoking_status']==i]['stroke'].value_counts().head(2)))
 
-print("Stroke frequency of all patients:")
-print(patients['stroke'].value_counts(), "\n")
+print("Stroke frequency for all the patients:")
+print(healthcare['stroke'].value_counts(), "\n")
 
-# Make a graph too
+test_healthcare = healthcare.copy()
+test_healthcare.drop(columns=['hypertension','age','id','gender','heart_disease','ever_married', 'work_type', 'Residence_type', 'avg_glucose_level', 'bmi'], inplace=True)
+strokers = test_healthcare[test_healthcare['stroke']==1]
+non_strokers = test_healthcare[test_healthcare['stroke']==0]
+strokers = pd.DataFrame(strokers)
+non_strokers = pd.DataFrame(non_strokers)
+print("*** Correlation between smoking and strokes ***\n\nPeople who had a stroke:")
+print(strokers['smoking_status'].value_counts(),"\n")
+print("People who hadn't a stroke:")
+print(non_strokers['smoking_status'].value_counts(),"\n")
+
+all_boyz = [strokers, non_strokers]
+good_data = pd.concat(all_boyz)
+sns.catplot(x="smoking_status", y="stroke", kind="bar",data=good_data)
+
+plt.xlabel('Smoking Status', fontsize=14)
+plt.ylabel('Percentage of Strokes (%)', fontsize=14)
+plt.show()
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# Copy dataset to change data (round ages, bmi, glucose levels)
+# Copy dataset to change data (round the ages, bmi, glucose levels)
 
-better_patients = patients.copy()
-better_patients['age'] = better_patients['age'].round(-1)
-better_patients['avg_glucose_level'] = better_patients['avg_glucose_level'].round(-1)
-better_patients['bmi'] = better_patients['bmi'].round(-1)
+better_healthcare = healthcare.copy()
+better_healthcare['age'] = better_healthcare['age'].round(-1)
+better_healthcare['avg_glucose_level'] = better_healthcare['avg_glucose_level'].round(-1)
+better_healthcare['bmi'] = better_healthcare['bmi'].round(-1)
 # Save to csv
-better_patients.to_csv("better_dataset.csv")
+better_healthcare.to_csv("better_dataset.csv")
 
-# Plot some graphs about the patients (display frequency of characteristics)
+# # Plot some graphs about the healthcare (display frequency of characteristics)
 
-# patients['gender'].value_counts().plot.bar()
+# # healthcare['gender'].value_counts().plot.bar()
+# # plt.show()
+# sns.countplot(better_healthcare['gender'])
 # plt.show()
-sns.countplot(better_patients['gender'])
-plt.show()
 
-# better_patients['age'].value_counts().plot.bar()
+# # better_healthcare['age'].value_counts().plot.bar()
+# # plt.show()
+# sns.countplot(better_healthcare['age'])
 # plt.show()
-sns.countplot(better_patients['age'])
-plt.show()
 
-sns.countplot(better_patients['hypertension'])
-plt.show()
-sns.countplot(better_patients['heart_disease'])
-plt.show()
-sns.countplot(better_patients['ever_married'])
-plt.show()
-sns.countplot(better_patients['work_type'])
-plt.show()
-sns.countplot(better_patients['Residence_type'])
-plt.show()
-sns.countplot(better_patients['avg_glucose_level'])
-plt.show()
-sns.countplot(better_patients['bmi'])
-plt.show()
-sns.countplot(better_patients['smoking_status'])
-plt.show()
-sns.countplot(better_patients['stroke'])
-plt.show()
-
-# plt.plot(patients.age, patients.bmi)
-
-# plt.xlabel('Ages')
-# plt.ylabel('BMI')
+# Add plot settings
+# sns.countplot(better_healthcare['hypertension'])
+# plt.show()
+# sns.countplot(better_healthcare['heart_disease'])
+# plt.show()
+# sns.countplot(better_healthcare['ever_married'])
+# plt.show()
+# sns.countplot(better_healthcare['work_type'])
+# plt.show()
+# sns.countplot(better_healthcare['Residence_type'])
+# plt.show()
+# sns.countplot(better_healthcare['avg_glucose_level'])
+# plt.show()
+# sns.countplot(better_healthcare['bmi'])
+# plt.show()
+# sns.countplot(better_healthcare['smoking_status'])
+# plt.show()
+# sns.countplot(better_healthcare['stroke'])
 # plt.show()
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-# How to replace display cols:
-# # mappings to standardize and clean the values
+# [ Random notes - Delete later ]
+
+# # How to replace display cols: with mappings to standardize and clean the values
 # mappings = {'MALE' : 'Male', 'FEMALE' : 'Female', 'Male(Child)' : 'Boy', 'Female(Child)' : 'Girl'}
 # # replace values using the defined mappings
-# data['SEX'] = data['SEX'].replace(mappings)
-# data['SEX'].value_counts()
+# data['gender'] = data['gender'].replace(mappings)
+# data['gender'].value_counts()
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
